df09f22c32ad50ab8e91faf92a53027c9b7403a3
micaelnisa <mikassantosnisa@gmail.com>
2023-12-28 22:14:19 +0000

    correcao

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+//dados para aceder à api
+
 const chaveAPI = 'a9a83ee2bf5765e9c4fcb9594ed08ae0';
 const nomeUtilizador = 'micaelnisa';
 const urlAPI = `https://ws.audioscrobbler.com/2.0/?method=user.getTopAlbums&user=${nomeUtilizador}&api_key=${chaveAPI}&format=json`;
 const semImagem = 'https://via.placeholder.com/300';
 
+//array de todos os dados dos albuns
 let todosAlbuns = [];
+//array dos albuns a ser exibidos na filtragem 
 let albunsExibidos = [];
 
 fetch(urlAPI)
@@ -12,15 +16,16 @@ fetch(urlAPI)
         todosAlbuns = data.topalbums.album;
         albunsExibidos = todosAlbuns.slice(); // Clonar o array
         exibirAlbuns(albunsExibidos);
-        popularFiltroArtista(todosAlbuns);
+        filtroTodosArtista(todosAlbuns);
     })
-    .catch(error => console.error('Erro ao obter dados da API do Last.fm:', error));
+    .catch(error => console.error('Erro', error));
 
 function exibirAlbuns(albuns) {
     const containerTopAlbuns = document.getElementById('topAlbuns');
     containerTopAlbuns.innerHTML = '';
 
     albuns.forEach(album => {
+        //dados para cada album
         const imagemAlbum = album.image[3]['#text'];
         const nomeAlbum = album.name;
         const nomeArtista = album.artist.name;
@@ -30,6 +35,7 @@ function exibirAlbuns(albuns) {
         const elementoAlbum = document.createElement('div');
         elementoAlbum.classList.add('album');
 
+        //o que vai ser inserido no hmtl
         const urlImagem = imagemAlbum ? imagemAlbum : semImagem;
         elementoAlbum.innerHTML = `
         <div> 
@@ -52,7 +58,7 @@ function exibirAlbuns(albuns) {
         containerTopAlbuns.appendChild(elementoAlbum);
     });
 }
-
+//ordem dos albuns
 function ordenarAlbuns() {
     const ordemSelecionada = document.getElementById('ordem').value;
     const albunsParaOrdenar = albunsExibidos.length > 0 ? albunsExibidos : todosAlbuns;
@@ -63,22 +69,22 @@ function ordenarAlbuns() {
         albunsParaOrdenar.sort((a, b) => b.playcount - a.playcount);
     }
 
-    albunsExibidos = albunsParaOrdenar.slice(); // Clonar o array ordenado
+    albunsExibidos = albunsParaOrdenar.slice(); 
     exibirAlbuns(albunsExibidos);
 }
-
-function popularFiltroArtista(albuns) {
+//filtro por artista
+function filtroTodosArtista(albuns) {
     const filtroArtista = document.getElementById('filtroArtista');
-    const artistasUnicos = [...new Set(albuns.map(album => album.artist.name))];
+    const artistas = [...new Set(albuns.map(album => album.artist.name))];
 
-    artistasUnicos.forEach(artista => {
+    artistas.forEach(artista => {
         const opcao = document.createElement('option');
         opcao.value = artista;
         opcao.textContent = artista;
         filtroArtista.appendChild(opcao);
     });
 }
-
+//filtro por artista asc e desc
 function filtrarPorArtista() {
     const artistaSelecionado = document.getElementById('filtroArtista').value;
 
@@ -90,10 +96,3 @@ function filtrarPorArtista() {
 
     exibirAlbuns(albunsExibidos);
 }
-
-
-
-
-
-
-let reproducoes = 0;
